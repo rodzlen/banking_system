e761378e3fdd05d2420d9b5926dd45d60712c016
rodzlen <qkqhrkxdkdy@naver.com>
2024-11-27 18:01:54 +0900

    구현완료

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,2 +1,8 @@
-def validate_transaction(func:callable)->callable:
-        return True if amount > 0 else False
+from typing import Callable # Callable은 임포트 해줘야함 
+
+def validate_transaction(func:Callable)->Callable:
+        def wrapper(self, amount:int)-> None:
+                if amount <= 0:
+                        raise ValueError("금액은 0보다 커야 합니다.")
+                return func(self, amount)
+        return wrapper
