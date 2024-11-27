e200b5a08b01bb454df8e2532696ee83af1792cb
rodzlen <qkqhrkxdkdy@naver.com>
2024-11-27 18:01:32 +0900

    유저목록 추가

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
-from account import Account
+from models.account import Account
 class User():
     def __init__(self, username:str) -> None:
+        self.users =[] # 유저 목록 저장
         self.username= username
         self.account = Account()
-        pass
