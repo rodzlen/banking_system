9997aafb2f742e44444296658cd53eb423ba53a7
rodzlen <qkqhrkxdkdy@naver.com>
2024-11-27 21:34:05 +0900

    Account 클래스 import

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 from models.account import Account
 class User():
     def __init__(self, username:str) -> None:
-        self.users =[] # 유저 목록 저장
-        self.username= username
+        self.username = username
         self.account = Account()
