535f8724064c1fdbedb4f07f31b40a66678cae10
rodzlen <qkqhrkxdkdy@naver.com>
2024-11-27 16:24:45 +0900

    예외처리 기본 틀 구현

---
+++ utils/exceptions.py
@@ -0,0 +1,11 @@
+class InsufficientFundsError:
+    def __init__(self, balance:int) -> None:
+        pass
+
+class NegativeAmountError:
+    def __init__(self) -> None:
+        pass
+
+class UserNotFoundError:
+    def __init__(self, username:str) -> None:
+        pass
